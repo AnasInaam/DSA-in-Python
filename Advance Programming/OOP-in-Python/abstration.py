07223afa31290970ac55212d2c0ec5ab5b821359
Mohammad Anas <mirzaanas937@gmail.com>
2025-09-19 12:07:36 +0530

    Refactor arithmetic operations into separate classes and implement a user input-driven calculator with a menu for addition and subtraction.

--- Advance Programming/OOP-in-Python/abstration.py
+++ Advance Programming/OOP-in-Python/abstration.py
@@ -29,20 +29,149 @@
 # pass : it is used to indicate that a block of code is intentionally left empty    
 
 
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
 
-class arithmeticOp(ABC):   # abstract bas class which contain min one abdstract class
-    @abstractmethod   # this is decorater 
-    def __init__(self , a ,b):
-        pass
-class Add(arithmeticOp):
-    def __init__(self ,a ,b):
-        print("This is the Addition" , a+b)
+# class arithmeticOp(ABC):   # abstract bas class which contain min one abdstract class
+#     @abstractmethod   # this is decorater 
+#     def __init__(self , a ,b):
+#         pass
+# class Add(arithmeticOp):
+#     def __init__(self ,a ,b):
+#         print("This is the Addition" , a+b)
 
-class sub(arithmeticOp):
-    def __init__(self ,a , b):
-        print("this is the substration" , abs(a-b))
+# class sub(arithmeticOp):
+#     def __init__(self ,a , b):
+#         print("this is the substration" , abs(a-b))
 
-# usage
-a = Add(10 , 20)
-b = sub(10 , 20 )
+# # usage
+# a = Add(10 , 20)
+# b = sub(10 , 20 )
+
+
+# from abc import ABC, abstractmethod
+
+# class arithmeticOp(ABC):   # abstract bas class which contain min one abdstract class
+#     @abstractmethod   # this is decorater 
+#     def calc(self):
+#         pass
+# class Add(arithmeticOp):
+#     def __init__(self ,a ,b):
+#         self.a = a 
+#         self.b = b
+#     def calc(self):
+#         print("This is the Addition" , a+b)
+
+# class sub(arithmeticOp):
+#     def __init__(self ,a ,b):
+#         self.a = a 
+#         self.b = b
+#     def calc(self ):
+#         print("this is the substration" , abs(a-b))
+
+# # usage
+# a = Add(10 , 20)
+# a.calc()
+# b = sub(10 , 20 )
+# b.calc()
+
+# from abc import ABC, abstractmethod
+
+# class arithmeticOp(ABC):   # abstract bas class which contain min one abdstract class
+#     @abstractmethod   # this is decorater 
+#     def calc(self):
+#         pass
+# class Add(arithmeticOp):
+#     def __init__(self ,a ,b):
+#         self.a = a 
+#         self.b = b
+#     def calc(self):
+#         print("This is the Addition" , a+b)
+# class res:
+#     def result(self , a , b):
+#         print(f"this is the {a} and {b} values")
+        
+# class sub(arithmeticOp , res):
+#     def __init__(self ,a ,b):
+#         self.a = a 
+#         self.b = b
+#     def calc(self ):
+#         print("this is the substration" , abs(a-b))
+#         self.result(a , b)
+
+
+# # usage
+# a = Add(10 , 20)
+# a.calc()
+# b = sub(10 , 20 )
+# b.calc()
+
+
+
+
+# class arithmeticOp:   # abstract bas class which contain min one abdstract class
+#     def calc(self):
+#         pass
+# class Add:
+#     def __init__(self ,a ,b):
+#         self.a = a 
+#         self.b = b
+#     def calc(self):
+#         print("This is the Addition" , self.a+self.b)
+# class res:
+#     def result(self , a , b):
+#         print(f"this is the {a} and {b} values")
+        
+# class sub(res):
+#     def __init__(self ,a ,b):
+#         self.a = a 
+#         self.b = b
+#     def calc(self ):
+#         print("this is the substration" , abs(self.a-self.b))
+#         self.result(self.a , self.b)
+
+# # usage
+# a = Add(10 , 20)
+# a.calc()
+# b = sub(10 , 20 )
+# b.calc()
+
+
+
+class Add:
+    def add(self, a, b):
+        print("Addition of numbers is:", a + b)
+
+class Sub:
+    def sub(self, a, b):
+        print("Subtraction of numbers is:", a - b)
+
+class UserInput:
+    def get_input(self):
+        a = int(input("Enter Num: "))
+        b = int(input("Enter Num: "))
+        return a, b
+
+class Calc(Sub, Add, UserInput):
+    def calc(self):
+        while True:
+            print("\nMenu")
+            print("For Addition press : 1")
+            print("For Subtraction press : 2")
+            print("For Exit press : 3")
+            choice = int(input("Enter the Choice: "))
+            match choice:
+                case 1:
+                    a, b = self.get_input()
+                    self.add(a, b)
+                case 2:
+                    a, b = self.get_input()
+                    self.sub(a, b)
+                case 3:
+                    print("Exiting...")
+                    exit()
+                case _:
+                    print("Invalid Choice")
+
+# run
+c = Calc()
+c.calc()
