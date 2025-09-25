e4a1392434c24477fe0dfc9a9febe8f8171e5294
Mohammad Anas <mirzaanas937@gmail.com>
2025-09-25 14:34:32 +0530

    Add memoized Fibonacci implementation

---
+++ Advance Programming/Advance-DSA.py/DP/fibbyDP.py
@@ -0,0 +1,10 @@
+def fib(n , memo):
+    if n in memo:
+        return memo
+    if n <= 1:
+        return n
+    memo[n] = fib(n-1 , memo) + fib(n-2 , memo)
+    return memo[n]
+
+n = 7
+print(fib(n))
